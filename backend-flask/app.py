90a0cb02ae0343beb06b9ea2a35ab649e1ea8afc
OpeOginni <brightoginni123@gmail.com>
2023-03-30 16:31:38 +0000

    Added Health-Check Endpoint

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -102,8 +102,9 @@ cors = CORS(
 #     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #     return response
 
-# MIDDLEWARE
-
+@app.route('/api/health-check')
+def health_check():
+  return {'success': True}, 200
 
 @app.route("/api/message_groups", methods=['GET'])
 # Using my verify_jwt middleware
