5e69c4d3d658670f1a3d3bc888f664e232be33ed
OpeOginni <brightoginni123@gmail.com>
2023-04-05 09:20:43 +0000

    Fixed Expiring Token

--- frontend-react-js/src/pages/NotificationsFeedPage.js
+++ frontend-react-js/src/pages/NotificationsFeedPage.js
@@ -52,8 +52,8 @@ export default function NotificationsFeedPage() {
     dataFetchedRef.current = true;
 
     loadData();
-    checkAuth();
-  }, [])
+    checkAuth(setUser);
+}, [])
 
   return (
     <article>
