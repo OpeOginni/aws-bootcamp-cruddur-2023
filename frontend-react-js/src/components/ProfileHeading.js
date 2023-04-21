0d71415be4fb8dc24cf7eff81893cc48ef1cb571
OpeOginni <brightoginni123@gmail.com>
2023-04-21 12:12:32 +0000

    Implemented Migrations, Profile Form, User Update Endpoint

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -27,9 +27,9 @@ export default function ProfileHeading(props) {
       <div className='display_name'>{props.profile.display_name}</div>
       <div className='handle'>@{props.profile.handle}</div>
       </div>
-
       <EditProfileButton setPopped={props.setPopped} />
    </div>
+   <div className='bio'>{props.profile.bio}</div>
          </div>
 
 );
