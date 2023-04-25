6b9c6870f2675fa90576fc1bdb2ca9c6f00b0752
OpeOginni <brightoginni123@gmail.com>
2023-04-25 16:33:08 +0000

    Removed Console.logs and edited s3 cors and erb

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -8,7 +8,6 @@ export default function ProfileForm(props) {
   const [displayName, setDisplayName] = React.useState(0);
 
   React.useEffect(()=>{
-    console.log('useEffects',props)
     setBio(props.profile.bio);
     setDisplayName(props.profile.display_name);
   }, [props.profile])
@@ -16,13 +15,13 @@ export default function ProfileForm(props) {
   const s3uploadkey = async (event)=> {
     try {
       console.log('s3 upload')
-      const backend_url = "https://9atlubq6ag.execute-api.eu-central-1.amazonaws.com/avatars/key_upload"
+      const backend_url = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT_URL}/avatars/key_upload`
       await getAccessToken()
       const access_token = localStorage.getItem("access_token")
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
-          'Origin': 'https://3000-opeoginni-awsbootcampcr-6rk8dyjkjfs.ws-eu95.gitpod.io',
+          'Origin': process.env.REACT_APP_FRONTEND_URL,
           'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
@@ -65,7 +64,6 @@ export default function ProfileForm(props) {
           'Content-Type': type
         }
       });
-      let data = await res.json();
       if (res.status === 200) {
 
       } else {
--- frontend-react-js/src/lib/CheckAuth.js
+++ frontend-react-js/src/lib/CheckAuth.js
@@ -17,7 +17,6 @@ export const checkAuth = async (setUser) => {
       bypassCache: false 
     })
     .then((cognito_user) => {
-      console.log('cognito_user',cognito_user);
       setUser({
         display_name: cognito_user.attributes.name,
         handle: cognito_user.attributes.preferred_username
@@ -25,7 +24,6 @@ export const checkAuth = async (setUser) => {
       return Auth.currentSession()
     }).then((cognito_user_session) => {
       localStorage.setItem("access_token", cognito_user_session.accessToken.jwtToken)
-
     })
     .catch((err) => console.log(err));
   };
