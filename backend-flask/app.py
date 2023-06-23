6b33e2e7c9648e4a47751e900a8b32594ecc68f4
OpeOginni <brightoginni123@gmail.com>
2023-06-23 18:31:17 +0000

    Finished up initialization refctor

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,8 +1,16 @@
-from flask import Flask
-from flask import request, g
-from flask_cors import CORS, cross_origin
 import os
 import sys
+
+from flask import Flask
+from flask import request, g
+from flask_cors import cross_origin
+
+from lib.rollbar import init_rollbar
+from lib.xray import init_xray
+from lib.honeycomb import init_honeycomb
+from lib.cors import init_cors
+from lib.cognito_jwt_token import jwt_required
+from lib.cloudwatch import init_cloudwatch
 
 from services.users_short import *
 from services.home_activities import *
@@ -17,85 +25,30 @@ from services.create_message import *
 from services.show_activity import *
 from services.update_profile import *
 
-from lib.cognito_jwt_token import jwt_required
-
-# HoneyComb --------
-from opentelemetry import trace
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
-
-# X-RAY --------
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
-
-# CLOUDWATCH LOGS --------
-import watchtower
-import logging
-from time import strftime
-
 # MIDDLEWARE
 from middleware.jwt_middleware import verify_jwt
-
-# Configuring Logger to Use CloudWatch
-# LOGGER = logging.getLogger(__name__)
-# LOGGER.setLevel(logging.DEBUG)
-# console_handler = logging.StreamHandler()
-# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
-# LOGGER.addHandler(console_handler)
-# LOGGER.addHandler(cw_handler)
-# LOGGER.info("Test Log")
-
-# HoneyComb --------
-# Initialize tracing and an exporter that can send data to Honeycomb
-provider = TracerProvider()
-processor = BatchSpanProcessor(OTLPSpanExporter())
-provider.add_span_processor(processor)
-
-# X-RAY --------
-# xray_url = os.getenv("AWS_XRAY_URL")
-# xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-
-
-# Shows the logs within the backend-flask app
-# simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-# provider.add_span_processor(simple_processor)
-
-trace.set_tracer_provider(provider)
-tracer = trace.get_tracer(__name__)
 
 # IMPORTANT PYTHON APP DECLARATION
 app = Flask(__name__)
 
-# X-RAY --------
-# XRayMiddleware(app, xray_recorder)
+# initialization -------------
+init_xray(app)
+# with app.app_context():
+#   init_rollbar()
 
-# HoneyComb --------
-# Initialize automatic instrumentation with Flask
-FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
-
-
-frontend = os.getenv('FRONTEND_URL')
-backend = os.getenv('BACKEND_URL')
-origins = [frontend, backend]
-cors = CORS(
-  app, 
-  resources={r"/api/*": {"origins": origins}},
-  headers=['Content-Type', 'Authorization'], 
-  expose_headers='Authorization',
-  methods="OPTIONS,GET,HEAD,POST"
-)
+init_honeycomb(app)
+init_cors(app)
 
 # Used to log errors to AWS CloudWatch
 # @app.after_request
 # def after_request(response):
-#     timestamp = strftime('[%Y-%b-%d %H:%M]')
-#     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-#     return response
+
+
+def model_json(model):
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
 
 @app.route('/api/health-check')
 def health_check():
@@ -110,10 +63,7 @@ def data_message_groups():
 
   if request.claims is not None: # If statement for my verify_juwt middleware
     model = MessageGroups.run(cognito_user_id=g.cognito_user_id)
-    if model['errors'] is not None:
-      return model['errors'], 422
-    else:
-      return model['data'], 200
+    return model_json(model)
   else:
     app.logger.debug('unauthenticated')
     data = HomeActivities.run()
@@ -126,14 +76,11 @@ def data_message_groups():
 def data_messages(message_group_uuid):
 
   if request.claims is not None: # If statement for my verify_juwt middleware
-      model = Messages.run(
-        cognito_user_id=g.cognito_user_id,
-        message_group_uuid=message_group_uuid
-        )
-      if model['errors'] is not None:
-        return model['errors'], 422
-      else:
-        return model['data'], 200
+    model = Messages.run(
+      cognito_user_id=g.cognito_user_id,
+      message_group_uuid=message_group_uuid
+      )
+    return model_json(model)
   else:
     app.logger.debug('unauthenticated')
     data = HomeActivities.run()
@@ -168,17 +115,14 @@ def data_create_message():
         message_group_uuid=message_group_uuid,
         cognito_user_id=g.cognito_user_id
         )
-    if model['errors'] is not None:
-      return model['errors'], 422
-    else:
-      return model['data'], 200
+    return model_json(model)
   else:
     app.logger.debug('unauthenticated')
     data = HomeActivities.run()
     return {}, 401
 
 def default_home_feed(e):
-  #   # unathenticated request
+  # unathenticated request
   app.logger.debug(e)
   app.logger.debug("unauthenicated")
   data = HomeActivities.run()
@@ -190,31 +134,7 @@ def default_home_feed(e):
 @verify_jwt("home_activities_endpoint")
 @jwt_required(on_error=default_home_feed)
 def data_home():
-
-  # Old Method
-
-  # access_token = extract_access_token(request.headers)
-  # try:
-  #   claims = cognito_jwt_token.verify(access_token)
-  #   # authenticated request
-  #   app.logger.debug("authenicated")
-  #   app.logger.debug(claims)
-  #   app.logger.debug(claims['username'])
-  #   data = HomeActivities.run(cognito_user_id=claims['username'])
-  # except TokenVerifyError as e:
-  #   # unathenticated request
-  #   app.logger.debug(e)
-  #   app.logger.debug("unauthenicated")
-  #   data = HomeActivities.run()
-  #   #data = HomeActivities.run(Logger=LOGGER)
-  # return data, 200
-
-  # data = HomeActivities.run(request.claims)
-  # app.logger.debug("########################")
-  # app.logger.debug(request.claims)
-
   data = HomeActivities.run(cognito_user_id=g.cognito_user_id)
-
   return data, 200
 
 
@@ -226,20 +146,13 @@ def data_notifications():
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
 def data_handle(handle):
   model = UserActivities.run(handle)
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
+  return return_model(model)
 
 @app.route("/api/activities/search", methods=['GET'])
 def data_search():
   term = request.args.get('term')
   model = SearchActivities.run(term)
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
-  return
+  return model_json(model)
 
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
@@ -252,10 +165,7 @@ def data_activities():
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
   # model = CreateActivity.run(message, g.cognito_user_id, ttl)
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
+  return model_json(model)
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
 def data_show_activity(activity_uuid):
@@ -268,11 +178,7 @@ def data_activities_reply(activity_uuid):
   user_handle  = 'andrewbrown'
   message = request.json['message']
   model = CreateReply.run(message, user_handle, activity_uuid)
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
-  return
+  return model_json(model)
 
 @app.route("/api/users/@<string:handle>/short", methods=['GET'])
 def data_users_short(handle):
@@ -296,10 +202,7 @@ def data_update_profile():
     bio=bio,
     display_name=display_name
     )
-  if model['errors'] is not None:
-      return model['errors'], 422
-  else:
-    return model['data'], 200
+  return model_json(model)
 
 if __name__ == "__main__":
   app.run(debug=True)
