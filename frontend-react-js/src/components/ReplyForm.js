62b2a7aea3b3d80ed8651fd73679977280b74ed5
OpeOginni <brightoginni123@gmail.com>
2023-06-25 20:29:41 +0000

    Started fixing the Replies

--- frontend-react-js/src/components/ReplyForm.js
+++ frontend-react-js/src/components/ReplyForm.js
@@ -1,6 +1,7 @@
 import './ReplyForm.css';
 import React from "react";
 import process from 'process';
+import { getAccessToken} from '../lib/CheckAuth'
 // import {ReactComponent as BombIcon} from './svg/bomb.svg';
 
 import ActivityContent  from '../components/ActivityContent';
@@ -19,6 +20,8 @@ export default function ReplyForm(props) {
     event.preventDefault();
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${props.activity.uuid}/reply`
+      await getAccessToken()
+      const access_token = localStorage.getItem('access_token')
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
