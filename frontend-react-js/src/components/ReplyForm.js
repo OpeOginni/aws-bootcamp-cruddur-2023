cb71f95be358af321dd80d31c56a6cba08c8cd09
OpeOginni <brightoginni123@gmail.com>
2023-07-02 16:44:20 +0000

    Started Activity Show page Video

--- frontend-react-js/src/components/ReplyForm.js
+++ frontend-react-js/src/components/ReplyForm.js
@@ -1,90 +1,83 @@
-import './ReplyForm.css';
+import "./ReplyForm.css";
 import React from "react";
-import process from 'process';
-import { post } from 'lib/Requests'
+import process from "process";
+import { post } from "lib/Requests";
 // import {ReactComponent as BombIcon} from './svg/bomb.svg';
 
-import ActivityContent  from 'components/ActivityContent';
-import FormErrors  from 'components/FormErrors';
+import ActivityContent from "components/ActivityContent";
+import FormErrors from "components/FormErrors";
 
 export default function ReplyForm(props) {
   const [count, setCount] = React.useState(0);
-  const [message, setMessage] = React.useState('');
+  const [message, setMessage] = React.useState("");
   const [errors, setErrors] = React.useState([]);
 
-  const classes = []
-  classes.push('count')
-  if (240-count < 0){
-    classes.push('err')
+  const classes = [];
+  classes.push("count");
+  if (240 - count < 0) {
+    classes.push("err");
   }
 
   const onsubmit = async (event) => {
-    event.preventDefault();  
-    const url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${props.activity.uuid}/reply`
+    event.preventDefault();
+    const url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${props.activity.uuid}/reply`;
 
     const payload_data = {
       activity_uuid: props.activity.uuid,
-      message: message
-    }
-    
-    post(url, payload_data,setErrors, function(data){
-      let activities_deep_copy = JSON.parse(JSON.stringify(props.activities))
-      let found_activity = activities_deep_copy.find(function (element) {
-        return element.uuid ===  props.activity.uuid;
-      });
-      found_activity.replies.push(data)
+      message: message,
+    };
 
-      props.setActivities(activities_deep_copy);
+    post(url, payload_data, setErrors, function (data) {
+      // let activities_deep_copy = JSON.parse(JSON.stringify(props.activities));
+      // let found_activity = activities_deep_copy.find(function (element) {
+      //   return element.uuid === props.activity.uuid;
+      // });
+
+      // found_activity.replies.push(data);
+
+      // props.setActivities(activities_deep_copy);
       // reset and close the form
-      setCount(0)
-      setMessage('')
-      props.setPopped(false)
-    })   
-  }
+      setCount(0);
+      setMessage("");
+      props.setPopped(false);
+    });
+  };
 
   const textarea_onchange = (event) => {
     setCount(event.target.value.length);
     setMessage(event.target.value);
-  }
+  };
 
   let content;
-  if (props.activity){
+  if (props.activity) {
     content = <ActivityContent activity={props.activity} />;
   }
 
-  const close = (event)=> {
+  const close = (event) => {
     if (event.target.classList.contains("reply_popup")) {
-      props.setPopped(false)
+      props.setPopped(false);
     }
-  }
+  };
 
   if (props.popped === true) {
     return (
       <div className="popup_form_wrap reply_popup" onClick={close}>
         <div className="popup_form">
           <div className="popup_heading">
-            <div className='popup_title'>
-            Reply to...
-
-            </div>
+            <div className="popup_title">Reply to...</div>
           </div>
           <div className="popup_content">
-            <div className="activity_wrap">
-              {content}
-            </div>
-            <form 
-              className='replies_form'
-              onSubmit={onsubmit}
-            >
+            <div className="activity_wrap">{content}</div>
+            <form className="replies_form" onSubmit={onsubmit}>
               <textarea
                 type="text"
                 placeholder="what is your reply?"
                 value={message}
-                onChange={textarea_onchange} 
+                onChange={textarea_onchange}
               />
-              <div className='submit'>
-                <div className={classes.join(' ')}>{240-count}</div>
-                <button type='submit'>Reply</button>
+              <div className="submit">
+                <div className={classes.join(" ")}>{240 - count}</div>
+                <button type="submit">Reply</button>
               </div>
               <FormErrors errors={errors} />
             </form>
