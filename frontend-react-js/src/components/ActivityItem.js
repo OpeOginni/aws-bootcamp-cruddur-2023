784ca8cc3a49a04b0fa629e7ec65c685ce58595b
OpeOginni <brightoginni123@gmail.com>
2023-07-05 23:20:07 +0000

    Fixed up Date/Time display, Activity Show and Migrations

--- frontend-react-js/src/components/ActivityItem.js
+++ frontend-react-js/src/components/ActivityItem.js
@@ -1,26 +1,58 @@
-import './ActivityItem.css';
+import "./ActivityItem.css";
 
-import { Link } from "react-router-dom";
+import ActivityContent from "../components/ActivityContent";
+import ActivityActionReply from "../components/ActivityActionReply";
+import ActivityActionRepost from "../components/ActivityActionRepost";
+import ActivityActionLike from "../components/ActivityActionLike";
+import ActivityActionShare from "../components/ActivityActionShare";
 
-import ActivityContent  from '../components/ActivityContent';
-import ActivityActionReply  from '../components/ActivityActionReply';
-import ActivityActionRepost  from '../components/ActivityActionRepost';
-import ActivityActionLike  from '../components/ActivityActionLike';
-import ActivityActionShare  from '../components/ActivityActionShare';
+import { useNavigate } from "react-router-dom";
 
 export default function ActivityItem(props) {
+  const navigate = useNavigate();
 
+  const click = (event) => {
+    event.preventDefault();
+    const url = `/@${props.activity.handle}/status/${props.activity.uuid}`;
+    navigate(url);
+    return false;
+  };
+  let expanded_meta;
+
+  if (props.expanded === true) {
+  }
+
+  const attrs = {};
+  if (props.expanded === true) {
+    attrs.className = "activity_item expanded";
+  } else {
+    attrs.className = "activity_item clickable";
+    attrs.onClick = click;
+  }
   return (
-    <Link className='activity_item' to={`/@${props.activity.handle}/status/${props.activity.uuid}`}>
-      <div className='activity_main'>
-      <ActivityContent activity={props.activity} />
-      <div className="activity_actions">
-        <ActivityActionReply setReplyActivity={props.setReplyActivity} activity={props.activity} setPopped={props.setPopped} activity_uuid={props.activity.uuid} count={props.activity.replies_count}/>
-        <ActivityActionRepost activity_uuid={props.activity.uuid} count={props.activity.reposts_count}/>
-        <ActivityActionLike activity_uuid={props.activity.uuid} count={props.activity.likes_count}/>
-        <ActivityActionShare activity_uuid={props.activity.uuid} />
+    <div {...attrs}>
+      <div className="activity_main">
+        <ActivityContent activity={props.activity} />
+        {expanded_meta}
+        <div className="activity_actions">
+          <ActivityActionReply
+            setReplyActivity={props.setReplyActivity}
+            activity={props.activity}
+            setPopped={props.setPopped}
+            activity_uuid={props.activity.uuid}
+            count={props.activity.replies_count}
+          />
+          <ActivityActionRepost
+            activity_uuid={props.activity.uuid}
+            count={props.activity.reposts_count}
+          />
+          <ActivityActionLike
+            activity_uuid={props.activity.uuid}
+            count={props.activity.likes_count}
+          />
+          <ActivityActionShare activity_uuid={props.activity.uuid} />
+        </div>
       </div>
-      </div>
-    </Link>
+    </div>
   );
 }
